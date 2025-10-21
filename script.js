7281ba5eebd53f60d5e91a907258de6491a8e3a8
Lazy-CSharp-Coder <harald.nydal@gmail.com>
2025-10-21 10:36:15 +0200

    test

---
+++ script.js
@@ -0,0 +1,17 @@
+// const data = 
+// {
+//     houseStark : {
+//                     Eddard : { nickname : "Ned", title : "Hand of the King"                          },
+//                     Caitlyn : { nickname : "Cat", title : "unknown"}
+
+//                  },
+//     houseLannister : { 
+//                        Tywin : { nickname : null, }
+
+
+//                      }
+
+// };
+
+const data = fetch("houses.json");
+console.log(data);
